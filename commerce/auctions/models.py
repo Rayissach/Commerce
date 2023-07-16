46bdc887d2fe3f2fc4ef65f1954053e00172dac7
Raymond Robinson <rayissach@gmail.com>
2023-07-15 21:43:27 -0500

    modify models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,18 +6,17 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=10,decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
-    pass
+    # pass
 
 class Bids(models.Model):
-    item = models.CharField()
-    price = models.ForeignKey()
-    bidders = models.ForeignKey()
+    item = models.CharField(max_length=20)
+    price = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid_price")
+    # bidders = models.ForeignKey()
     bid = models.DecimalField(max_digits=10, decimal_places=2)
-    
-    pass
+    # pass
 
 class Comments(models.Model):
     pass
