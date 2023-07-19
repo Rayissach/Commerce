84cf0a8af3c237cd31f1dfc225d188c58bc4bb62
Raymond Robinson <rayissach@gmail.com>
2023-07-19 09:04:42 -0500

    Rebuilding database and recreating superuser

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -18,14 +18,12 @@ class Listing(models.Model):
     image = models.ImageField(height_field=60, width_field=60)
     created = models.DateTimeField(auto_now_add=True)
     categories = models.ManyToManyField(Categories, related_name="categories")
-    # pass
 
 class Bids(models.Model):
     item = models.CharField(max_length=20)
     price = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid_price")
     bidders = models.PositiveIntegerField()
     bid = models.DecimalField(max_digits=10, decimal_places=2)
-    # pass
 
 class Comments(models.Model):
     pass
